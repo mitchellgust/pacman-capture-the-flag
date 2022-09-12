f1e39ed09ff17854e14e82b94394a00c0a30f3b2
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-09-12 22:13:28 +1000

    add notes for implementation ideas

--- myTeam.py
+++ myTeam.py
@@ -45,19 +45,61 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
+
+# Common Commands:
+# 
+# Run against baseline team:
+# python capture.py -r myTeam -b baselineTeam
+# 
+# Run against itself:
+# python capture.py -r myTeam -b myTeam
+# 
+# Run with different layout:
+# 
+# python capture.py -r myTeam -b baselineTeam -l officeCapture
+# ---------------
+
 class OffensiveReflexAgent(CaptureAgent):
   def registerInitialState(self, gameState):
     self.start = gameState.getAgentPosition(self.index)
+    
+    # TODO: Initialize variables we need here:
+    # positions to ghosts, food, walls, and capsules
+    
     CaptureAgent.registerInitialState(self, gameState)
-
+  
   def chooseAction(self, gameState):
+    # TODO: Get information about the gameState
+    # Where is the closest food?
+    # Where is the closest ghost?
+    # Where is the closest capsule?
+    # Once we have decided what to do, we can call aStarSearch to find the best action
+    
     return
 
 
 class DefensiveReflexAgent(CaptureAgent):
   def registerInitialState(self, gameState):
     self.start = gameState.getAgentPosition(self.index)
+    
+    # TODO: Initialize variables we need here:
+    # positions to food, enemy pacman, capsules and the entrances of 
+    # the ally's side of the maze
+    
     CaptureAgent.registerInitialState(self, gameState)
 
   def chooseAction(self, gameState):
+    # TODO: Get information about the gameState
+    # Is the agent scared? Should we stay away from the enemy but as close as possible?
+    # Are there any enemies within 5 steps of the agent? Chase them!
+    # Is there any food that was present in the last observation that 
+    # was eaten? Go to that location.
+    # 
+    # Once we have decided what to do, we can call aStarSearch to find the best action
     return
+  
+  def aStarSearch():
+    # TODO: Implement aStarSearch algorithm here with manhattan distance as heuristic
+    # Return the best action in aStarSearch
+    return
+
